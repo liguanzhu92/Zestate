3c27d8063034d0bb99334271ea0a41bf5b8ee71b
shashankreddykuchukulla <skuchukulla@student.govst.edu>
2017-01-21 13:06:39 -0600

    project base refactor

--- app/src/main/java/com/guanzhuli/zestate/controller/VolleyController.java
+++ app/src/main/java/com/guanzhuli/zestate/controller/VolleyController.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.text.TextUtils;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 /**
